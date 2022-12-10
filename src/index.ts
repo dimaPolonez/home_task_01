7f69ee875e074ed9a6b383d853a6db84cf0090b4
dimaPolonez <dima-barxat@yandex.ru>
2022-12-10 21:05:06 +0300

    feat: add response all

--- src/index.ts
+++ src/index.ts
@@ -8,31 +8,35 @@ const parserMiddleware = express.json()
 app.use(parserMiddleware)
 
 let bdVideos = [
-    { id : +(new Date()), title: "Video0", author : "Author0",
+    { id : 1, title: "Video0", author : "Author0",
         canBeDownloaded: true, minAgeRestriction : null,
     createdAt : "2022-12-10T10:03:20.216Z",
     publicationDate : "publicationDate",
     availableResolutions : [ "P144" ]
     },
-    { id : +(new Date()), title: "Video1", author : "Author1",
+    { id : 2, title: "Video1", author : "Author1",
         canBeDownloaded: true, minAgeRestriction : null,
         createdAt : "2022-12-10T10:03:20.216Z",
         publicationDate : "publicationDate",
         availableResolutions : [ "P144" ]
     },
-    { id : +(new Date()), title: "Video2", author : "Author2",
+    { id : 3, title: "Video2", author : "Author2",
         canBeDownloaded: true, minAgeRestriction : null,
         createdAt : "2022-12-10T10:03:20.216Z",
         publicationDate : "publicationDate",
         availableResolutions : [ "P144" ]
     },
-    { id : +(new Date()), title: "Video3", author : "Author3",
+    { id : 4, title: "Video3", author : "Author3",
         canBeDownloaded: true, minAgeRestriction : null,
         createdAt : "2022-12-10T10:03:20.216Z",
         publicationDate : "publicationDate",
         availableResolutions : [ "P144" ]
     },
 ]
+
+app.get('/', (req: Request, res: Response) => {
+    res.json('Hello, server start!')
+})
 
 app.post('/hometask_01/api/videos', (req: Request, res: Response) => {
 
@@ -52,16 +56,59 @@ app.get('/hometask_01/api/videos', (req: Request, res: Response) => {
     res.json(bdVideos)
 })
 
-app.get('/hometask_01/api/videos/{id}', (req: Request, res: Response) => {
-    res.send('Hello Incubator! I am Backend-developer!')
+app.get('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
+    const findId = bdVideos.find(v => v.id === +req.params.id)
+
+    if (!findId) {
+        res.sendStatus(404)
+        return;
+    }
+
+    res.json(findId)
 })
 
-app.put('/hometask_01/api/videos/{id}', (req: Request, res: Response) => {
-    res.send('Hello Incubator! I am Backend-developer!')
+app.put('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
+    let findId = bdVideos.find(v => v.id === +req.params.id)
+
+    if (!findId) {
+        res.sendStatus(404)
+        return;
+    }
+
+/*    if ((req.body.title && req.body.author && req.body.availableResolutions && req.body.canBeDownloaded &&
+        req.body.minAgeRestriction && req.body.publicationDate) != undefined) {
+        findId.title = req.body.title
+        findId.author = req.body.author
+        findId.availableResolutions = req.body.availableResolutions
+        findId.canBeDownloaded = req.body.canBeDownloaded
+        findId.minAgeRestriction = req.body.minAgeRestriction
+        findId.publicationDate = req.body.publicationDate
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(400)
+    }*/
+
+    findId.title = req.body.title
+    findId.author = req.body.author
+    findId.availableResolutions = req.body.availableResolutions
+    findId.canBeDownloaded = req.body.canBeDownloaded
+    findId.minAgeRestriction = req.body.minAgeRestriction
+    findId.publicationDate = req.body.publicationDate
+    res.sendStatus(204)
+
+
 })
 
-app.delete('/hometask_01/api/videos/{id}', (req: Request, res: Response) => {
-    res.send('Hello Incubator! I am Backend-developer!')
+app.delete('/hometask_01/api/videos/:id', (req: Request, res: Response) => {
+
+    if (!req.params.id) {
+        res.sendStatus(404)
+        return;
+    }
+
+    bdVideos = bdVideos.filter(v => v.id !== +req.params.id)
+
+    res.sendStatus(204)
 })
 
 app.listen(port, () => {
